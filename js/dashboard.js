8c87f817402718a9ceeb879f8c24698726a07444
Emmanuel Onyeike <emmanuelonyeike33@gmail.com>
2026-01-03 00:49:14 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -74,21 +74,22 @@ const views = {
             </div>
             <i class="fas fa-crown absolute -bottom-4 -right-4 text-white/[0.02] text-8xl rotate-12 group-hover:text-orange-500/[0.05] transition-all"></i>
         </div>
-<div class="bg-[#050b1d] border border-white/5 p-6 rounded-[2rem] relative overflow-hidden group hover:border-blue-400/30 transition-all">
-    <div class="flex items-center gap-4 relative z-10">
-        <div class="w-12 h-12 bg-blue-400/10 rounded-2xl flex items-center justify-center border border-blue-400/20">
-            <i class="fas fa-graduation-cap text-blue-400"></i>
-        </div>
-        <div>
-            <p class="text-[10px] font-black text-gray-500 uppercase tracking-widest">Semester</p>
-            <h3 id="dash-semester-val" class="text-3xl font-black text-white mt-1">0</h3>
+  <div class="bg-[#050b1d] border border-white/5 p-6 rounded-[2rem] relative overflow-hidden group hover:border-red-500/30 transition-all">
+            <div class="flex items-center gap-4 relative z-10">
+                <div class="w-12 h-12 bg-red-600/10 rounded-2xl flex items-center justify-center border border-red-500/20">
+                    <i class="fas fa-fire text-red-500"></i>
+                </div>
+                <div>
+                    <p class="text-[10px] font-black text-gray-500 uppercase tracking-widest">CP Score</p>
+                    <h3 class="text-3xl font-black text-white mt-1">0</h3>
+                </div>
+            </div>
+            <i class="fas fa-terminal absolute -bottom-4 -right-4 text-white/[0.02] text-8xl rotate-12 group-hover:text-red-500/[0.05] transition-all"></i>
         </div>
     </div>
-    <i class="fas fa-graduation-cap absolute -bottom-4 -right-4 text-white/[0.02] text-8xl rotate-12 group-hover:text-blue-400/[0.05] transition-all"></i>
-</div>
 
 
-   
+
    <div class="mt-8 bg-[#050b1d] border border-white/5 p-8 rounded-[2.5rem] relative overflow-hidden">
     <div class="flex flex-col md:flex-row justify-between items-start md:items-center mb-6 gap-4">
         <div>
